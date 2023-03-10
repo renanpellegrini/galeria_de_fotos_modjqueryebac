a23ac460dbb84b5ef628219611fde3db7c7882fb
Renan Pellegrini <renanpellegrini@outlook.com>
2023-03-10 14:56:37 -0300

    commit 2

--- main.js
+++ main.js
@@ -10,5 +10,18 @@ $(document).ready(function() {
     $('form').on('submit', function(e) {
         console.log('submit');
         e.preventDefault();
-    })
+        const enderecoNovaImagem = $("#endereço-imagem-nova").val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoNovaImagem}"/>`).appendTo(novoItem);
+        $(`
+        <div class="overlay-imagem-link">
+            <a href="${enderecoNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+            Ver imagem em tamanho real
+            </a>
+        </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(500);
+        $('#endereço-imagem-nova').val('');
+        })
 })
